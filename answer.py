4d0c7a9e298d4639a92d27838c3e08e453101086
PIXMARCO <mortadhaammar@gmail.com>
2025-05-10 10:07:28 +0300

    Add files via upload

--- answer.py
+++ answer.py
@@ -2,6 +2,28 @@ import base64
 import uuid
 import os
 from elasticsearch import Elasticsearch
+from pydantic import BaseModel,Field , EmailStr
+from typing import List, Optional
+from datetime import datetime
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 es = Elasticsearch("http://192.168.10.102:9200")
 
@@ -47,9 +69,42 @@ index_mapping = {
     }
 }
 
-if es.indices.exists(index=index_name):
-    es.indices.delete(index=index_name)
-    print(f"Index '{index_name}' deleted.")
+# if es.indices.exists(index=index_name):
+#     # es.indices.delete(index=index_name)
+#     # print(f"Index '{index_name}' deleted.")
 
-es.indices.create(index=index_name, body=index_mapping)
-print(f"Index '{index_name}' created successfully.")
+
+
+class Author(BaseModel):
+    name: str
+    refrens_type: Optional[str]
+    tel: Optional[str]
+    email: Optional[EmailStr]
+    place: Optional[str]
+
+class Paragraph(BaseModel):
+    content: str
+    change_state: Optional[str]
+    files: Optional[List[str]]
+
+class QuestionRelation(BaseModel):
+    relations: Optional[str]
+    Province: Optional[str]
+    city: Optional[str]
+    files: Optional[str]
+    q_id: Optional[str]
+    relation_type: Optional[str]
+
+class AnswerDoc(BaseModel):
+    id: str
+    question: str
+    question_relation: Optional[QuestionRelation]
+    q_type: Optional[str]
+    title: Optional[str]
+    subject: Optional[str]
+    keyword: Optional[str]
+    paragraphs: Optional[List[Paragraph]]
+    author: Optional[Author]
+    timestamp :Optional[datetime] = Field(default_factory=lambda: datetime.now().isoformat())
+
+
